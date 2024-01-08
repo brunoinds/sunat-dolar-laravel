fc2d810d13f29356cfa5c65a3885d7b0d7fca1e3
Bruno Freire <brunoqm99@gmail.com>
2024-01-08 16:09:49 -0500

    Implements month-year API request

--- src/Converter/Converter.php
+++ src/Converter/Converter.php
@@ -9,23 +9,23 @@ use Carbon\Carbon;
 
 class Converter{
     public static function getDailyDollarExchangeRate(DateTime $date){
-        $response = self::fetchAPI($date);
+        $response = self::fetchMAPI($date);
         return $response;
     }
     public static function getDailySolesExchangeRate(DateTime $date){
-        $response = self::fetchAPI($date);
+        $response = self::fetchMAPI($date);
         $response->price->buy = (1 / $response->price->buy);
         $response->price->sell = (1 / $response->price->sell);
         return $response;
     }
     public static function convertDollarToSoles(DateTime $date, float $amount){
-        $response = self::getDailyDollarExchangeRate($date);
+        $response = self::fetchMAPI($date);
         $price = $response->price->sell;
         $result = $amount * $price;
         return $result;
     }
     public static function convertSolesToDollar(DateTime $date, float $amount){
-        $response = self::getDailyDollarExchangeRate($date);
+        $response = self::fetchMAPI($date);
         $price = $response->price->buy;
         $result = $amount / $price;
         return $result;
@@ -94,6 +94,90 @@ class Converter{
         Cache::store('file')->put('Brunoinds/SunatDolarLaravelStore', json_encode($stores));
         
         return $results;
+    }
+    private static function fetchMAPI(DateTime $date){
+        if ($date->format('Y-m-d') > Carbon::now()->timezone('America/Lima')->format('Y-m-d')){
+            $date = Carbon::now()->timezone('America/Lima')->toDateTime();
+        }
+
+
+        $dateString = $date->format('Y-m-d');
+        $monthString = $date->format('m');
+        $yearString = $date->format('Y');
+
+        $cachedValue = Cache::store('file')->get('Brunoinds/SunatDolarLaravelStore');
+
+        if ($cachedValue){
+            $stores = json_decode($cachedValue, true);
+            if (isset($stores[$dateString])){
+                return json_decode($stores[$dateString]);
+            }
+        }
+
+
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_URL => 'https://api.apis.net.pe/v1/tipo-cambio-sunat?year='.$yearString.'&month=' . $monthString,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSL_VERIFYPEER => 0,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 2,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => [
+                'Referer: https://apis.net.pe/tipo-de-cambio-sunat-api',
+            ],
+        ]);
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+        $results = json_decode($response, true);
+
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            if (str_contains($response, 'Not Found')){
+                return self::fetchAPI(Carbon::now()->timezone('America/Lima')->toDateTime());
+            }
+            throw new \Exception('Invalid JSON response: "' . json_last_error_msg(). '". The API response was: ' . $response);
+        }
+
+
+
+        $resultsParsed = [];
+        $result = null;
+        foreach ($results as $value) {
+            $resultParsed = [
+                'price' => [
+                    'buy' => $value['compra'],
+                    'sell' => $value['venta']
+                ],
+                'date' => DateTime::createFromFormat('Y-m-d', $value['fecha']),
+                'origin' => $value['origen']
+            ];
+
+            if ($value['fecha'] == $dateString){
+                $result = $resultParsed;
+            }
+
+            $resultsParsed[$value['fecha']] = $resultParsed;
+        }
+
+        if ($result === null){
+            throw new \Exception('Could not find the date in the API response: ' . $response);
+        }
+
+
+        foreach ($resultsParsed as $date => $result) {
+            $stores[$date] = json_encode($result);
+        }
+
+        Cache::store('file')->put('Brunoinds/SunatDolarLaravelStore', json_encode($stores));
+        
+
+        $result = json_decode(json_encode($result));
+        return $result;
     }
 
 }
